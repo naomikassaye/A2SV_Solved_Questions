7bc3f84a6b71e042b3dc3ab9d9a84a87f5390ee7
Naomi Kassaye Tuffa <naomikassaye18@gmail.com>
2026-03-17 20:56:25 +0300

    Add solution for Sum of Square Numbers

---
+++ leetcode/0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -0,0 +1,16 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        left=0
+        right=int(sqrt(c))
+        while left<=right:
+            sum=left*left+right*right
+            if sum==c:
+                return True
+            elif sum<c:
+                left+=1
+            else:
+                right-=1
+        return False
+
+        
+        
