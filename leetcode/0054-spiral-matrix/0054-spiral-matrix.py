456a6a30d29c80c9b6552a8b1480a9396c846bef
Naomi Kassaye Tuffa <naomikassaye18@gmail.com>
2026-03-24 16:01:10 +0300

    Add solution for Spiral Matrix

---
+++ leetcode/0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,26 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix:
+            return []
+        res=[]
+        t=0
+        l=0
+        b=len(matrix)-1
+        r=len(matrix[0])-1
+
+        while t<=b and l<=r:
+            for i in range(l,r+1):
+                res.append(matrix[t][i])
+            t+=1
+            for i in range(t,b+1):
+                res.append(matrix[i][r])
+            r-=1
+            if t<=b:
+                for i in range(r,l-1,-1):
+                    res.append(matrix[b][i])
+                b-=1
+            if l<=r:
+                for i in range(b, t-1, -1):
+                    res.append(matrix[i][l])
+                l+=1
+        return res
