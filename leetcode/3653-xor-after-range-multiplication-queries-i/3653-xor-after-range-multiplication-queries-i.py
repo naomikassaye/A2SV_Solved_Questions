118db63b9be1de5d953d458ad72e539649c0d4a8
Naomi Kassaye Tuffa <naomikassaye18@gmail.com>
2026-04-14 12:23:46 +0300

    Add solution for XOR After Range Multiplication Queries I

---
+++ leetcode/3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.py
@@ -0,0 +1,10 @@
+class Solution:
+    def xorAfterQueries(self, nums: List[int], queries: List[List[int]]) -> int:
+        M = 10**9 + 7
+        for l, r, k, v in queries:
+            for i in range(l, r + 1, k):
+                nums[i] = (nums[i] * v) % M
+        res = 0
+        for x in nums:
+            res ^= x
+        return res
