6f7128ae53c8c6c437285aef033e143e33323170
Naomi Kassaye Tuffa <naomikassaye18@gmail.com>
2026-04-25 19:19:02 +0300

    Add solution for N-Queens II

---
+++ leetcode/0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        ans=0
+        cols=set()
+        pos=set()
+        neg=set()
+        def backtrack(r):
+            nonlocal ans
+            if r==n:
+                ans+=1
+                return
+            for c in range(n):
+                if c in cols or (r+c) in pos or (r-c) in neg:
+                    continue
+                cols.add(c)
+                pos.add(r+c)
+                neg.add(r-c)
+                backtrack(r+1)
+                cols.remove(c)
+                pos.remove(r+c)
+                neg.remove(r-c)
+        backtrack(0)
+        return ans
